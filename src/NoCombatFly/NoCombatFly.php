3e8e5cacbab4be7d2b344968f960f1b86441b979
TheRealVerge <34665340+TheRealVerge@users.noreply.github.com>
2018-03-14 18:40:05 -0600

    Update NoCombatFly.php

--- src/NoCombatFly/NoCombatFly.php
+++ src/NoCombatFly/NoCombatFly.php
@@ -3,10 +3,7 @@
     /*
      * Plugin created by TheRealVerge
      */
-
-
 namespace NoCombatFly;
-
 //Events
 use pocketmine\event\Listener as L;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
@@ -30,45 +27,37 @@ class NoCombatFly extends PB implements L{
   public function onEnable(){
    $this->getServer()->getLogger()->info("NCFly by TheRealVerge enabled");
    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
 }
   public function onDisable(){
    $this->getServer()->getLogger()->info("NCFly by TheRealVerge disabled");
-
 }
     
     /*
      * @param EntityDamageEvent $event
      */
-
 	public function onDamage(EntityDamageEvent $event){
 		if($event instanceof EntityDamageByEntityEvent){
 			$human = $event->getEntity(); //Victim
 			$damager = $event->getDamager(); //Attacker
-
      if($human instanceof Player && $damager instanceof Player){
 		  if($damager->isFlying()){
 					$damager->setFlying(false);
 					$damager->sendMessage(TF::GRAY."(".TF::AQUA."!".TF::GRAY.")".TF::AQUA."Flight disabled in".TF::RED."Combat!");
-
 //HeartParticle
 $human->getLevel()->addParticle(new HeartParticle($human->getLocation()));
 //GhastSound
 $damager->getLevel()->addSound(new GhastSound($damager));
-
 }
 		 if($human->isFlying()){
 					$human->setFlying(false);
 					$human->sendMessage(TF::GRAY."(".TF::AQUA."!".TF::GRAY.")".TF::AQUA."Flight disabled in".TF::RED."Combat!");
-
 //HeartParticle
 $damager->getLevel()->addParticle(new HeartParticle($human->getLocation()));
 //GhastSound
 $human->getLevel()->addSound(new GhastSound($human));
-
 					
 				}
 			}
 		}
 	}
-}	
+}
